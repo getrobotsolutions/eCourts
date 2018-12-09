bf9677571cb59e7de1260fdf4cdc51864305f920
tamoorkayani <tamoorkayani@gmail.com>
2018-12-10 03:18:54 +0500

    update
    update

--- Contents/SlideShow/js/info.js
+++ Contents/SlideShow/js/info.js
@@ -2,7 +2,7 @@
 //Redirects to homepage after 1 minute of not interaction
 //-----------------------------------------------------
 
-function redirect(){
+/*function redirect(){
     window.location.href = "../../maincontents.htm";
 }
 var initial=setTimeout(redirect,60000);
@@ -10,7 +10,7 @@ var initial=setTimeout(redirect,60000);
 $(document).click(function(event) {
     clearTimeout( initial );
     initial=setTimeout(redirect,60000);
-});
+});*/
 //-----------------------------------------------------
 
 
